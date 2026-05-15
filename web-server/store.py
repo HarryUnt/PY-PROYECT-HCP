de4e3693efa7045f92a93e90087ccecf0e5a07c8
HarryUnt <harrycape2015@gmail.com>
2026-05-15 14:13:17 -0500

    Nuevoa añadidos al proyecto y actualización de los requisitos de las carpetas app y web-server

--- web-server/store.py
+++ web-server/store.py
@@ -1,13 +1,11 @@
 import requests
 
-api_url_categories = "https://api.escuelajs.co/api/v1/categories"
-
 
 def get_categories():
-    r = requests.get(api_url_categories)
-    print(f"Status code: {r.status_code}")
-    print(f"Text: {r.text}")
-    print(f"type: {type(r.text)}")
+    r = requests.get("https://api.escuelajs.co/api/v1/categories")
+    print(r.status_code)
+    print(r.text)
+    print(type(r.text))
     categories = r.json()
     for category in categories:
         print(category["name"])
